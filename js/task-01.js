af9938a01fb465d282c6b24855ccfc931c099616
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-03 18:13:54 +0300

    task 1 done

---
+++ js/task-01.js
@@ -0,0 +1,10 @@
+const listOfCategories = document.querySelector('ul#categories');
+// console.log(listOfCategories);
+const listOfItems = listOfCategories.querySelectorAll('li.item');
+// console.log(listOfItems);
+console.log('Number of categories: ', listOfItems.length);
+
+listOfItems.forEach(item => {
+    console.log("Category: ", item.firstElementChild.textContent);
+    console.log("Elements: ", item.querySelectorAll('li').length);
+});
