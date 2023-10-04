1b5fc1e9f4417490971220d91536a85d6021a712
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-04 18:54:49 +0300

    task 2 done, 3 need to finish

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,14 @@
-const listOfCategories = document.querySelector('ul#categories');
-// console.log(listOfCategories);
-const listOfItems = listOfCategories.querySelectorAll('li.item');
-// console.log(listOfItems);
-console.log('Number of categories: ', listOfItems.length);
+function consoleList() {
+    const listOfCategories = document.querySelector('ul#categories');
+    // console.log(listOfCategories);
+    const listOfItems = listOfCategories.querySelectorAll('li.item');
+    // console.log(listOfItems);
+    console.log('Number of categories: ', listOfItems.length);
 
-listOfItems.forEach(item => {
-    console.log("Category: ", item.firstElementChild.textContent);
-    console.log("Elements: ", item.querySelectorAll('li').length);
-});
+    listOfItems.forEach(item => {
+        console.log("Category: ", item.firstElementChild.textContent);
+        console.log("Elements: ", item.querySelectorAll('li').length);
+    });
+}
+
+consoleList();
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+function createList() {
+  const listOfIngredientd = document.querySelector('ul#ingredients');
+  const markup = ingredients.map(ingredient => {
+    const item = document.createElement('li');
+    item.textContent = ingredient;
+    item.classList.add('item');
+    return item;
+  })
+  return listOfIngredientd.append(...markup);
+}
+
+createList();
