82a0f15426190ca955b7e78e155a5b11851feb4a
OnishYurii <onishchenkoyurii@gmail.com>
2023-10-07 14:27:06 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector("button.change-color");
+const indicateColor = document.querySelector("span.color");
+const body = document.querySelector("body");
+
+changeColorBtn.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  indicateColor.textContent = randomColor;
+}
